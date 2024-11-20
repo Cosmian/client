5dc3509dfe8df155659705e51c607aea226ba342
Manuthor <manu.coste@gmail.com>
2024-11-20 11:17:37 +0100

    ci: try fix ubuntu 24.04

--- .github/scripts/cargo_build.ps1
+++ .github/scripts/cargo_build.ps1
@@ -31,8 +31,18 @@ function BuildProject {
     }
     Get-ChildItem ..\..
 
+    # Build at root
+    if ($BuildType -eq "release") {
+        cargo build --release --target x86_64-pc-windows-msvc
+    }
+    else {
+        cargo build --target x86_64-pc-windows-msvc
+    }
+
     # Check binaries
-    Get-ChildItem -Recurse target\x86_64-pc-windows-msvc\$BuildType
+    Get-ChildItem target\x86_64-pc-windows-msvc\$BuildType
+    Get-ChildItem target
+    Get-ChildItem target\$BuildType
 
     # Check dynamic links
     $output = & "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.29.30133\bin\HostX64\x64\dumpbin.exe" /dependents target\x86_64-pc-windows-msvc\$BuildType\cosmian.exe | Select-String "libcrypto"
