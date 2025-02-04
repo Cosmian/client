ef7f1da9bd099a08bfccbfec42c3761bba28f0d1
Manuthor <manu.coste@gmail.com>
2025-02-04 10:42:10 +0100

    ci: test push_to_package_cosmian

--- .github/scripts/cargo_build.sh
+++ .github/scripts/cargo_build.sh
@@ -83,8 +83,8 @@ export RUST_LOG="cosmian_cli=trace,cosmian_findex_client=trace,cosmian_kmip=erro
 # shellcheck disable=SC2086
 cargo test --target $TARGET $RELEASE $FEATURES --workspace -- --nocapture $SKIP_SERVICES_TESTS
 
-while true; do
-  sleep 1 && reset
-  # shellcheck disable=SC2086
-  cargo test --target $TARGET $RELEASE $FEATURES --workspace -- --nocapture $SKIP_SERVICES_TESTS
-done
+# while true; do
+#   sleep 1 && reset
+#   # shellcheck disable=SC2086
+#   cargo test --target $TARGET $RELEASE $FEATURES --workspace -- --nocapture $SKIP_SERVICES_TESTS
+# done
