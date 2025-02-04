9f66e1ce9743a3a154ff5d6238c2598ed062fd50
Manuthor <manu.coste@gmail.com>
2025-02-04 11:02:40 +0100

    ci: test push_to_package_cosmian

--- .github/scripts/push_to_package_cosmian.sh
+++ .github/scripts/push_to_package_cosmian.sh
@@ -6,6 +6,20 @@ set -ex
 
 DEBUG_OR_RELEASE="$1"
 
+apt update -y
+apt-get install -y zip
+
+for archive_name in $ARCHIVE_NAMES; do
+  zip -r "$archive_name".zip "$archive_name"
+done
+
+# Warning, no windows binaries in debug
+if [ "${DEBUG_OR_RELEASE}" = "release" ]; then
+  zip -r "$archive_name".zip windows-release
+fi
+
+find . # List zip files
+
 if [[ "${GITHUB_REF}" =~ 'refs/tags/' ]]; then
   BRANCH="${GITHUB_REF_NAME}"
 else
@@ -13,30 +27,32 @@ else
 fi
 
 DESTINATION_DIR=/mnt/package/cli/$BRANCH
-KMS_DESTINATION_DIR=/mnt/package/kms/4.22.0
-FINDEX_SERVER_DESTINATION_DIR=/mnt/package/findex-server/0.2.0
 
 ssh -o 'StrictHostKeyChecking no' -i /root/.ssh/id_rsa cosmian@package.cosmian.com mkdir -p "$DESTINATION_DIR"
-scp -o 'StrictHostKeyChecking no' -i /root/.ssh/id_rsa \
-  "*.zip" \
-  cosmian@package.cosmian.com:"$DESTINATION_DIR"/
+scp -o 'StrictHostKeyChecking no' -i /root/.ssh/id_rsa "*.zip" cosmian@package.cosmian.com:"$DESTINATION_DIR"/
 
-if [ "$DEBUG_OR_RELEASE" = "release" ]; then
-  ssh -o 'StrictHostKeyChecking no' -i /root/.ssh/id_rsa cosmian@package.cosmian.com mkdir -p "$DESTINATION_DIR"/{rhel9,ubuntu-20.04,ubuntu-22.04,ubuntu-24.04}
+# Push the packages to the package.cosmian.com server
+# if [ "$DEBUG_OR_RELEASE" = "release" ]; then
+KMS_DESTINATION_DIR=/mnt/package/kms/4.22.1
+FINDEX_SERVER_DESTINATION_DIR=/mnt/package/findex-server/0.2.0
 
-  # RedHat 9 package
-  for dir in "$DESTINATION_DIR/rhel9" "$KMS_DESTINATION_DIR/rhel9" "$FINDEX_SERVER_DESTINATION_DIR/rhel9"; do
+ssh -o 'StrictHostKeyChecking no' -i /root/.ssh/id_rsa cosmian@package.cosmian.com mkdir -p "$DESTINATION_DIR"/{rhel9,ubuntu-20.04,ubuntu-22.04,ubuntu-24.04}
+
+# RedHat 9 package
+for dir in "$DESTINATION_DIR/rhel9" "$KMS_DESTINATION_DIR/rhel9" "$FINDEX_SERVER_DESTINATION_DIR/rhel9"; do
+  scp -o 'StrictHostKeyChecking no' \
+    -i "/root/.ssh/id_rsa rhel9-$DEBUG_OR_RELEASE/generate-rpm/*.rpm" \
+    cosmian@package.cosmian.com:"$dir"
+done
+
+# Ubuntu packages
+for version in 20.04 22.04 24.04; do
+  for dir in "$DESTINATION_DIR/ubuntu-$version" "$KMS_DESTINATION_DIR/ubuntu-$version" "$FINDEX_SERVER_DESTINATION_DIR/ubuntu-$version"; do
     scp -o 'StrictHostKeyChecking no' \
-      -i "/root/.ssh/id_rsa rhel9-$DEBUG_OR_RELEASE/generate-rpm/*.rpm" \
+      -i "/root/.ssh/id_rsa ubuntu_${version//./_}-$DEBUG_OR_RELEASE/debian/*.deb" \
       cosmian@package.cosmian.com:"$dir"
   done
+done
 
-  # Ubuntu packages
-  for version in 20.04 22.04 24.04; do
-    for dir in "$DESTINATION_DIR/ubuntu-$version" "$KMS_DESTINATION_DIR/ubuntu-$version" "$FINDEX_SERVER_DESTINATION_DIR/ubuntu-$version"; do
-      scp -o 'StrictHostKeyChecking no' \
-        -i "/root/.ssh/id_rsa ubuntu_${version//./_}-$DEBUG_OR_RELEASE/debian/*.deb" \
-        cosmian@package.cosmian.com:"$dir"
-    done
-  done
-fi
+# end
+# fi
