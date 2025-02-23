6db7567a9ed24fd5149fd322b4e54d3cace92e1a
Manuthor <32013169+Manuthor@users.noreply.github.com>
2025-02-23 08:55:29 +0100

    feat: findex-server update: delegate encryption to KMS (#13)
    * fix: update findex and kms deps
    
    * test: fix binary test script
    
    * test: add test with hmac and aes-xts keys
    
    * docs: renew markdown auto doc
    
    * test: simplify code

--- .github/scripts/cosmian_tests.sh
+++ .github/scripts/cosmian_tests.sh
@@ -3,8 +3,11 @@
 set -ex
 
 export KMS_CLI_FORMAT=json
-SEED="11223344556677889900AABBCCDDEEFF11223344556677889900AABBCCDDEEFF"
 COSMIAN="cargo run --bin cosmian --"
+
+# Create the seed key
+seed_key_id=$($COSMIAN kms sym keys create | jq -r '.unique_identifier')
+echo "seed_key_id: $seed_key_id"
 
 # Create the key encryption key
 kek_id=$($COSMIAN kms sym keys create | jq -r '.unique_identifier')
@@ -15,10 +18,33 @@ index_id=$($COSMIAN findex-server permissions create | sed 's/Created Index ID: 
 echo "index_id: $index_id"
 
 # Encrypt and index the data
-$COSMIAN findex-server encrypt-and-index --seed $SEED --index-id "$index_id" --kek-id "$kek_id" --csv test_data/datasets/smallpop.csv
+$COSMIAN findex-server encrypt-and-index --seed-key-id "$seed_key_id" --index-id "$index_id" --kek-id "$kek_id" --csv test_data/datasets/smallpop.csv
 
 # Search and decrypt the data
-expected_line=$($COSMIAN findex-server search-and-decrypt --seed $SEED --index-id "$index_id" --kek-id "$kek_id" --keyword "Southborough" | sed 's/Decrypted records: //')
+expected_line=$($COSMIAN findex-server search-and-decrypt --seed-key-id "$seed_key_id" --index-id "$index_id" --kek-id "$kek_id" --keyword "Southborough" | sed 's/Decrypted records: //')
+echo "expected_line: $expected_line"
+
+# Check the result
+if [[ "$expected_line" != "[\"SouthboroughMAUnited States9686\"]" ]]; then
+  echo "Test failed: unexpected result"
+  exit 1
+else
+  echo "Test passed: local encryption"
+fi
+
+# Generate the HMAC key
+hmac_key_id=$($COSMIAN kms sym keys create | jq -r '.unique_identifier')
+echo "hmac_key_id: $hmac_key_id"
+
+# Generate the AES-XTS-512 key
+aes_xts_key_id=$($COSMIAN kms sym keys create | jq -r '.unique_identifier')
+echo "aes_xts_key_id: $aes_xts_key_id"
+
+# Encrypt and index the data
+$COSMIAN findex-server encrypt-and-index --hmac-key-id "$hmac_key_id" --aes-xts-key-id "$aes_xts_key_id" --index-id "$index_id" --kek-id "$kek_id" --csv test_data/datasets/smallpop.csv
+
+# Search and decrypt the data
+expected_line=$($COSMIAN findex-server search-and-decrypt --hmac-key-id "$hmac_key_id" --aes-xts-key-id "$aes_xts_key_id" --index-id "$index_id" --kek-id "$kek_id" --keyword "Southborough" | sed 's/Decrypted records: //')
 echo "expected_line: $expected_line"
 
 # Check the result
@@ -27,5 +53,6 @@ if [[ "$expected_line" != "[\"SouthboroughMAUnited States9686\"]" ]]; then
   exit 1
 else
   echo "Test passed"
-  exit 0
 fi
+
+exit 0
