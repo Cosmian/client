4dc60b09fb56691137c18f2826eb71eb22c0d53d
Manuthor <manu.coste@gmail.com>
2025-04-30 13:54:41 +0200

    build: ui

--- .github/scripts/build_ui.sh
+++ .github/scripts/build_ui.sh
@@ -40,4 +40,4 @@ cargo install --version 0.13.1 wasm-pack --force
 # Build WASM component
 cd ../crate/wasm
 # shellcheck disable=SC2086
-wasm-pack build --target web --release $FEATURES
+RUSTUP_TOOLCHAIN="nightly-2025-01-01" wasm-pack build --target web --release $FEATURES
